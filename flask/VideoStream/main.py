1c27ea4c1bc6d905566e9057677a491023e07696
Gabriel Soranzo <gabriel.soranzo@u-pec.fr>
2023-04-25 19:29:39 +0200

    flask basic NLP

--- flask/VideoStream/main.py
+++ flask/VideoStream/main.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, Response, request
 from cameraFlux import WebcamStream
 import pandas as pd
+from nlp import *
 
 
 # initializing and starting multi-threaded webcam input stream 
@@ -9,17 +10,46 @@ print("\nInitilisation du flux vidéo")
 webcam_stream = WebcamStream(stream_id=0) # 0 id for main camera
 webcam_stream.start()
 
+ordre=""
+action="Neither"
+
 app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
+    global ordre,action
+
     if request.method == "POST":
-        model = None
-        description = None
         try:
-            description = str(request.form["description"])
+            ordre=str(request.form["ordre"])
         except:
-            errors += "Valeur non valide dans le champ description.<br>"
+            ordre="Valeur non valide"
+            error="Valeur non valide dans le champ ordre.<br>"
+
+        if ordre!="":
+            result=findAction(ordre)
+            action=result[0][0]
+            score=result[1][0]
+            print("Action: ",result)
+            if score>0.3 and (action=="Take" or action=="Drop"):
+                description=findTarget(ordre,action)
+                print("Target: ", description)
+            else:
+                action=""
+                description="an object"
+        else:
+            description = None
+            try:
+                description = str(request.form["description"])
+            except:
+                errors += "Valeur non valide dans le champ description.<br>"
+            action=None
+            try:
+                action=str(request.form["action"])
+            except:
+                errors+= "Valeur non valide dans le champ action.<br>"
+
+        model = None
         try:
             model = str(request.form["model"])
         except:
@@ -29,6 +59,7 @@ def index():
                 webcam_stream.update_mask(description,None)
             else:
                 webcam_stream.update_mask(description,model)
+
     if webcam_stream.mask_model=="CLIPSeg":
         checked=["checked","","","",""]
     elif webcam_stream.mask_model=="CLIPSeg mask only":
@@ -39,7 +70,15 @@ def index():
         checked=["","","","checked",""]
     else:
         checked=["","","","","checked"]
-    return render_template('index.html', description=webcam_stream.mask_description,checked=checked)
+
+    if action=="Take":
+        checked_act=["checked","",""]
+    elif action=="Drop":
+        checked_act=["","checked",""]
+    else:
+        checked_act=["","","checked"]
+
+    return render_template('index.html', description=webcam_stream.mask_description,checked=checked,checked_act=checked_act,ordre=ordre)
 
 def gen():
     while True:
